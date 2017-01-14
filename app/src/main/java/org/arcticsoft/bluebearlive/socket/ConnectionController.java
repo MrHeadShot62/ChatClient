964affb4a87021f04b7ebe2a0850f35cd9565eb8
mrheadshot62 <headshotik62@gmail.com>
2017-01-14 18:41:03 +0200

    connection controller

--- app/src/main/java/org/arcticsoft/bluebearlive/socket/ConnectionController.java
+++ app/src/main/java/org/arcticsoft/bluebearlive/socket/ConnectionController.java
@@ -17,24 +17,41 @@ import java.net.Socket;
  */
 
 public class ConnectionController {
-
+    private static String TAG = "ServerConnection";
     private static BlueBearOutputStream output;
     private static BlueBearInputStream input;
     public static boolean isStarted = false;
+    public static boolean isConnected=false;
+
 
     public static boolean start(String ip){
-        if (!isStarted) {
-            try {
-                AsyncTask<Void, Void, Boolean> task = new ServerConnection(ip);
-                task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-                return task.get();
-            } catch (Exception e) {
-                Log.e("NewClientThread", "Не удалось подключиться к серверу");
-                return false;
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(Application.getInstance().getContext(), "Connection attempt", Toast.LENGTH_SHORT).show();
+                try{
+                    Log.d(TAG, "Connection attempt");
+                    Socket socket = new Socket(ip, 27015);
+                    ConnectionController.input = new BlueBearInputStream(socket.getInputStream());
+                    ConnectionController.output = new BlueBearOutputStream(socket.getOutputStream());
+                    Toast.makeText(Application.getInstance().getContext(), "Connected to server!", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, String.format("connected to %s", ip));
+                    ConnectionController.isStarted = true;
+                    ConnectionController.isConnected =true;
+                }catch(Exception e){
+                    Toast.makeText(Application.getInstance().getContext(), "Failed to connect", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, String.format("failed to connect %s", ip));
+                    ConnectionController.isStarted = false;
+                }
             }
-        }else{
-            return false;
+        });
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        return isConnected;
     }
 
     public static BlueBearInputStream getInput() {
@@ -43,55 +60,5 @@ public class ConnectionController {
 
     public static BlueBearOutputStream getOutput() {
         return output;
-    }
-
-    static class ServerConnection extends AsyncTask<Void, Void, Boolean>{
-        private String TAG = "ServerConnection";
-        private String ip;
-
-
-        public ServerConnection(String ip) {
-            this.ip = ip;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean started) {
-            super.onPostExecute(started);
-            if (started){
-                Toast.makeText(Application.getInstance().getContext(), "Connected to server!", Toast.LENGTH_LONG).show();
-                Log.d(TAG, String.format("connected to %s", ip));
-                ConnectionController.isStarted = true;
-            }else{
-                Toast.makeText(Application.getInstance().getContext(), "Failed to connect", Toast.LENGTH_LONG).show();
-                Log.d(TAG, String.format("failed to connect %s", ip));
-                ConnectionController.isStarted = false;
-            }
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-            new ServerListener(input).execute();
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... voids) {
-            try{
-                Log.d(TAG, "Connection attempt");
-                Socket socket = new Socket(ip, 27015);
-                ConnectionController.input = new BlueBearInputStream(socket.getInputStream());
-                ConnectionController.output = new BlueBearOutputStream(socket.getOutputStream());
-                publishProgress();
-            }catch(Exception e){
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            Toast.makeText(Application.getInstance().getContext(), "Connection attempt", Toast.LENGTH_SHORT).show();
-        }
     }
 }
