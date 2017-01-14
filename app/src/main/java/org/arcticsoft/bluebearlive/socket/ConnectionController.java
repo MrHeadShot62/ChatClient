49a3dc78c6538c38393ac914d0c0e05221b0772f
mrheadshot62 <headshotik62@gmail.com>
2017-01-14 18:55:27 +0200

    connection controller

--- app/src/main/java/org/arcticsoft/bluebearlive/socket/ConnectionController.java
+++ app/src/main/java/org/arcticsoft/bluebearlive/socket/ConnectionController.java
@@ -28,18 +28,15 @@ public class ConnectionController {
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(Application.getInstance().getContext(), "Connection attempt", Toast.LENGTH_SHORT).show();
                 try{
                     Log.d(TAG, "Connection attempt");
                     Socket socket = new Socket(ip, 27015);
                     ConnectionController.input = new BlueBearInputStream(socket.getInputStream());
                     ConnectionController.output = new BlueBearOutputStream(socket.getOutputStream());
-                    Toast.makeText(Application.getInstance().getContext(), "Connected to server!", Toast.LENGTH_LONG).show();
                     Log.d(TAG, String.format("connected to %s", ip));
                     ConnectionController.isStarted = true;
                     ConnectionController.isConnected =true;
                 }catch(Exception e){
-                    Toast.makeText(Application.getInstance().getContext(), "Failed to connect", Toast.LENGTH_LONG).show();
                     Log.d(TAG, String.format("failed to connect %s", ip));
                     ConnectionController.isStarted = false;
                 }
